9b77573488eb68198f605ec4e9580cd82c78d437
Tommy <tommydanks2000@outlook.com>
2024-05-22 23:32:57 +0100

    (fix): logo import

--- src/components/admin/sideNav/index.tsx
+++ src/components/admin/sideNav/index.tsx
@@ -11,7 +11,6 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "@/assets/logo.png";
 import { usePathname } from "next/navigation";
 
 const AdminSideNavigation = () => {
@@ -27,7 +26,7 @@ const AdminSideNavigation = () => {
           className="group flex size-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
           <Image
-            src={logo}
+            src={"/logo.png"}
             alt="Games Recaped Logo"
             width={100}
             height={100}
