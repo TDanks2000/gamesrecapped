3d461b95a304a4914f21d17c02bab22ce5bac132
Tommy <tommydanks2000@outlook.com>
2024-05-25 08:01:23 +0100

    (refactor): homepage views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ type PageProps = {
 export default async function Home({ searchParams }: PageProps) {
   return (
     <main className="flex min-h-screen flex-col gap-4 p-5 px-4 md:flex-row md:px-10">
-      <div className="order-2 flex w-full flex-col gap-4 px-4 md:order-1 md:w-2/3 md:px-0">
+      <div className="order-2 flex w-full flex-col gap-4 px-4 md:order-1 md:w-full md:px-0">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Games</h1>
           <div className="flex gap-2">
@@ -36,7 +36,7 @@ export default async function Home({ searchParams }: PageProps) {
           <GamesView searchParams={searchParams} />
         </Suspense>
       </div>
-      <div className="relative order-1 flex h-[600px] w-full justify-end md:order-2 md:w-1/3">
+      <div className="relative order-1 flex h-[600px] w-full justify-end md:order-2 md:w-[30%]">
         <div className="flex w-full flex-col gap-2 rounded-lg bg-muted/40 p-2">
           <h1 className="text-center text-lg font-bold">Conferences</h1>
           <Separator className="mb-2" />
