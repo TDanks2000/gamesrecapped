22670be08a3dddf95ade5630793d15c80140d081
Tommy <tommydanks2000@outlook.com>
2024-06-04 15:24:12 +0100

    (add): dynamic pride logo depending on month

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -18,6 +18,9 @@ const NavBar: FC = () => {
 
   if (pathname.startsWith("/admin")) return null;
 
+  // is month June
+  const logo = new Date().getMonth() === 5 ? "/logo-pride.png" : "/logo.png";
+
   return (
     <header className="flex h-16 items-center gap-4 border-b bg-muted/40 px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
@@ -26,7 +29,7 @@ const NavBar: FC = () => {
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Image
-            src={"/logo.png"}
+            src={logo}
             alt="Games Recaped Logo"
             width={50}
             height={50}
@@ -66,7 +69,7 @@ const NavBar: FC = () => {
               className="flex items-center gap-2 text-lg font-semibold"
             >
               <Image
-                src={"/logo.png"}
+                src={logo}
                 alt="Games Recaped Logo"
                 width={50}
                 height={50}
