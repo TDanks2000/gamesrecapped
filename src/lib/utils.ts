afd9ae84a5ea7656c873c64a99d1859fa9c4ffda
tommy <tommydanks2000@gmail.com>
2024-06-04 23:11:52 +0100

    (fix): link errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,16 +7,20 @@ export function cn(...inputs: ClassValue[]) {
 
 export const getImageFromURL = (link: string | null | undefined) => {
   if (!link) return null;
+  try {
+    const url = new URL(link);
+    const host = url.host.replaceAll("www.", "");
 
-  const url = new URL(link);
-  const host = url.host.replaceAll("www.", "");
-
-  switch (host) {
-    case "youtube.com":
-    case "youtu.be":
-      return `https://img.youtube.com/vi/${url.pathname.split("/").pop()}/hqdefault.jpg`;
-    default:
-      return link;
+    switch (host) {
+      case "youtube.com":
+      case "youtu.be":
+        return `https://img.youtube.com/vi/${url.pathname.split("/").pop()}/hqdefault.jpg`;
+      default:
+        return link;
+    }
+  } catch (error) {
+    console.error(error);
+    return "";
   }
 };
 
